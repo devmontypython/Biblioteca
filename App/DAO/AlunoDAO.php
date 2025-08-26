7fe3389f986752576691d36492a0b23ab7b39fd2
leo <devmontypython@gmail.com>
2025-08-26 00:00:09 -0300

    Configuração: Crud - DAO - Esqueleto

--- App/DAO/AlunoDAO.php
+++ App/DAO/AlunoDAO.php
@@ -1,5 +1,38 @@
 <?
 
+namespace App\DAO;
+
+use App\Model\Aluno;
+
 class AlunoDAO {
-    
+    public function save(Aluno $model) : Aluno {
+
+        if($model -> id == null) {
+            return $this -> insert($model);
+        } else {
+            return $this -> update($model);
+        }
+
+        // OPERADOR TERNÁRIO        
+    }
+
+    public function insert(Aluno $model) {
+
+    }
+
+    public function update(Aluno $model) {
+
+    }
+
+    public function selectById(int $id) {
+
+    }
+
+    public function select() {
+
+    }
+
+    public function delete(int $id) {
+
+    }
 }
